d2c4ad12f50465fad2a7e3444f9e6d8133928e8a
Yash Burshe <bursheyash2@gmail.com>
2025-03-04 03:47:06 -0500

    Add theme color meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import type { Viewport } from 'next'
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from '@vercel/analytics/next';
@@ -7,10 +8,14 @@ export const metadata: Metadata = {
   title: 'Yash Burshe',
   description: 'My slice of the internet',
   applicationName: 'Yash Burshe',
-  authors: {url: 'https://yashburshe.com', name: 'Yash Burshe'},
+  authors: { url: 'https://yashburshe.com', name: 'Yash Burshe' },
   generator: 'Next.js',
   creator: 'Yash Burshe',
   publisher: 'Yash Burshe',
+}
+
+export const viewport: Viewport = {
+  themeColor: '#0f172a',
 }
 
 export default function RootLayout({
@@ -24,8 +29,8 @@ export default function RootLayout({
       <body className="min-h-dvh">
         <div>
           {children}
-          <SpeedInsights/>
-          <Analytics/>
+          <SpeedInsights />
+          <Analytics />
         </div>
       </body>
     </html>
